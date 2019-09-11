32aeea0a133c74fb572d8a853dca975ab37319aa
Siarhei <spartateambuilder@mail.ru>
2019-09-11 12:23:42 +0300

    Updates

---
+++ precache-manifest.e70c2de3fbf690fad85a7ae6fe2bc2c5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1b7e39717488ee61b671b9e57689b8",
+    "url": "/portflio/index.html"
+  },
+  {
+    "revision": "ae9b95e275438047d042",
+    "url": "/portflio/static/css/main.57fd0f25.chunk.css"
+  },
+  {
+    "revision": "281bc102ff79a35e9795",
+    "url": "/portflio/static/js/2.305b618e.chunk.js"
+  },
+  {
+    "revision": "ae9b95e275438047d042",
+    "url": "/portflio/static/js/main.0e27f8b6.chunk.js"
+  },
+  {
+    "revision": "b2db2e317854528550c4",
+    "url": "/portflio/static/js/runtime~main.80326728.js"
+  },
+  {
+    "revision": "d3d29912cac8f2d2b6f1dc6444bed6f4",
+    "url": "/portflio/static/media/Avatar.d3d29912.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portflio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4bf41a96b690bf715eae267c91ecf611",
+    "url": "/portflio/static/media/logo3.4bf41a96.png"
+  },
+  {
+    "revision": "6b88793d20388f5c07569027c4e8572a",
+    "url": "/portflio/static/media/todolist.6b88793d.png"
+  }
+]);
